2067d648a9c6a9acc7b9f1aba9604ef3ab47f742
Oğuz <oguzerol91@gmail.com>
2019-02-01 16:41:07 +0300

    react helmet added

--- imports/ui/pages/homepage.js
+++ imports/ui/pages/homepage.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import { Helmet } from 'react-helmet';
 export default class HomePage extends React.Component {
   render() {
     return (
-      <div>Homepage</div>
+      <>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Home Page</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+        <div>Homepage</div>
+      </>
     );
   }
 }
